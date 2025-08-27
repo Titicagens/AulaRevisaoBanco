d6fff187e6d61fdae3930de5816d79b48f45ef4c
Titicagens <willianweber123@gmail.com>
2025-08-27 19:58:23 -0300

    Primeiro Commit
    verdade

---
+++ AulaRevisaoBanco/AulaRevisaoBanco/src/main/kotlin/enumeradores/Material.kt
@@ -0,0 +1,5 @@
+package enumeradores
+
+enum class Material {
+    PVC, PLÁSTICO, METAL, FITA
+}
---
+++ AulaRevisaoBanco/AulaRevisaoBanco/src/main/kotlin/ui/Menu.kt
@@ -0,0 +1,8 @@
+package org.example.ui
+
+fun menu(){
+    println("1 - Cadastrar CaixaD'Água")
+    println("1 - Editar CaixaD'Água")
+    println("1 - Listar CaixaD'Água")
+    println("1 - Editar CaixaD'Água")
+}
